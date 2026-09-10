1cded88929a705bdbf64b4e73273b544f7c07c4a
Muhamad Soleh <solehudinz3692@gmail.com>
2026-09-11 06:10:07 +0700

    Fix photo path formatting in kepalaDinas object

--- src/data/pegawaiData.js
+++ src/data/pegawaiData.js
@@ -11,7 +11,7 @@ export const pegawaiList = [
 
 export const kepalaDinas = {
   nama: "Muhamad Soleh, S.Pd",
-  foto: "/img/konfigurasi/pimpinan/msoleh.png)",
+  foto: "/img/konfigurasi/pimpinan/msoleh.png",
   sambutan: `Selamat datang di Website kami Dinas Pemuda Olahraga dan Kebudayaan Kabupaten Lembata. Website ini dimaksudkan sebagai sarana publikasi untuk memberikan informasi dan gambaran Dinas Pemuda Olahraga dan Kebudayaan Kabupaten Lembata dalam hal publikasi kepada masyarakat.
 Dan sebagai wujud rasa tanggung jawab kami dalam rangka meningkatkan pelayanan kepada masyarakat, maka kami adakan website dinas ini. Kritik dan saran terhadap kekurangan dan kesalahan yang ada sangat kami harapkan guna penyempurnaan website ini di masa akan datang. Terima kasih.`,
 };
