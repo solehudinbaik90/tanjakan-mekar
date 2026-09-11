9b1273e11c8da981911f7590d6a098886233edd5
Muhamad Soleh <solehudinz3692@gmail.com>
2026-09-11 20:08:11 +0700

    Refactor Home component layout and structure
    Refactor Home component layout for better responsiveness and structure. Updated class names and added slide-inner div for improved styling.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,57 +26,73 @@ export default function Home() {
     <div className="container">
       {/* Slider banner utama */}
       <div className="row">
-        <div className="col-md-12">
+        <div className="col-md-12 col-sm-12">
           <SliderWrapper className="mb-0">
             {homeSliderBanner.map((b, i) => (
               <div className="item-slide jl_radus_e" key={i}>
-                <Link to={b.to}>
-                  <img src={b.image} title={b.title} alt={b.title} className="img-fluid" />
-                </Link>
+                <div className="slide-inner">
+                  <Link to={b.to}>
+                    <img src={b.image} title={b.title} alt={b.title} className="img-fluid" />
+                  </Link>
+                </div>
               </div>
             ))}
           </SliderWrapper>
         </div>
 
         {/* Marquee pengumuman */}
-        <div className="col-md-12 mb-3">
-          <div className="info-dinas-header">
-            <span className="re-info">Pengumuman <i className="fas fa-bullhorn text-light" /></span>
-          </div>
-          <div className="dinas-info col-md-12">
-            <marquee onMouseOver={(e) => e.target.stop()} onMouseOut={(e) => e.target.start()}>
-              {pengumumanList.map((p) => (
-                <span key={p.id} style={{ marginRight: 40 }}>
-                  <span style={{ color: "#f5f5f5", background: "orange", padding: "3px 5px" }}>
-                    {p.tanggal}
-                  </span>{" "}
-                  <span className="pointer">{p.judul}</span>
-                </span>
-              ))}
-            </marquee>
+        <div className="col-md-12 col-sm-12 mb-3">
+          <div id="pengumuman" className="pengumuman">
+            <div className="info-dinas-header">
+              <span className="re-info">Pengumuman <i className="fas fa-bullhorn text-light" /></span>
+            </div>
+            <div className="dinas-info col-md-12">
+              <marquee className="item" onMouseOver={(e) => e.target.stop()} onMouseOut={(e) => e.target.start()}>
+                {pengumumanList.map((p) => (
+                  <span key={p.id} style={{ marginRight: 40 }}>
+                    <span style={{ color: "#f5f5f5", background: "orange", padding: "3px 5px" }}>
+                      {p.tanggal}
+                    </span>{" "}
+                    <span className="pointer">{p.judul}</span>
+                  </span>
+                ))}
+              </marquee>
+            </div>
           </div>
         </div>
       </div>
 
       {/* Berita utama + 4 berita samping */}
       <div className="row">
-        <div className="col-md-8">
-          <div className="jl_m_center_w jl_radus_e" style={{ position: "relative" }}>
-            <div
-              className="jl_f_img_bg"
-              style={{ backgroundImage: `url(${beritaUtama.image})`, height: 360, backgroundSize: "cover" }}
-            />
-            <Link to={`/${beritaUtama.slug}`} className="jl_f_img_link" />
-            <div className="text-box">
-              <span className="badge badge-primary">
-                <Link to={`/category/${beritaUtama.kategoriSlug}`}>{beritaUtama.kategori}</Link>
-              </span>
-              <span className="post-date pl-2" style={{ color: "#305b90" }}>{beritaUtama.tanggal}</span>
-              <h3><Link to={`/${beritaUtama.slug}`}>{beritaUtama.judul}</Link></h3>
+        <div className="col-md-8 col-sm-12">
+          <div className="jl_m_center blog-style-one blog-small-grid">
+            <div className="jl-w-slider jl_full_feature_w">
+              <div className="item-slide jl_m_center_w jl_radus_e">
+                <div className="slide-inner">
+                  <div className="jl_m_center_w jl_radus_e">
+                    <div
+                      className="jl_f_img_bg"
+                      style={{ backgroundImage: `url(${beritaUtama.image})` }}
+                    />
+                    <Link to={`/${beritaUtama.slug}`} className="jl_f_img_link" />
+                    <div className="text-box">
+                      <div className="d-flex align-items-center">
+                        <span className="badge badge-primary">
+                          <Link to={`/category/${beritaUtama.kategoriSlug}`}>{beritaUtama.kategori}</Link>
+                        </span>
+                        <span className="jl_post_meta pl-2 pb-3">
+                          <span className="post-date" style={{ color: "#305b90" }}> {beritaUtama.tanggal}</span>
+                        </span>
+                      </div>
+                      <h3><Link to={`/${beritaUtama.slug}`}>{beritaUtama.judul}</Link></h3>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <div className="col-md-4">
+        <div className="col-md-4 col-sm-12">
           {beritaSamping.map((b) => <BeritaCard item={b} key={b.slug} />)}
         </div>
       </div>
@@ -95,9 +111,16 @@ export default function Home() {
       <SliderWrapper className="mb-4">
         {midAdsBanner.map((b, i) => (
           <div className="item-slide jl_radus_e" key={i}>
-            <a href={b.href} target="_blank" rel="noreferrer" title={b.title}>
-              <img src={b.image} alt={b.title} className="img-fluid rounded" style={{ width: "100%" }} />
-            </a>
+            <div className="slide-inner">
+              <a href={b.href} target="_blank" rel="noreferrer" title={b.title}>
+                <img
+                  src={b.image}
+                  alt={b.title}
+                  className="img-fluid position-relative rounded"
+                  style={{ width: "100%", height: "auto" }}
+                />
+              </a>
+            </div>
           </div>
         ))}
       </SliderWrapper>
@@ -134,12 +157,18 @@ export default function Home() {
       <div className="row mb-4">
         {ebookList.map((eb) => (
           <div className="col-md-6 mb-4" key={eb.id}>
-            <div className="jl_topik_center_w jl_radus_e" style={{ position: "relative", height: 280 }}>
-              <div className="jl_f_img_bg" style={{ backgroundImage: `url(${eb.thumb})`, height: "100%", backgroundSize: "cover" }} />
-              <a href={eb.file} target="_blank" rel="noreferrer" className="jl_f_img_link" title="Baca Buku" />
-              <div className="text-box">
-                <span className="jl_f_cat"><a style={{ background: "#305b90" }} className="post-category-color-text">{eb.kategori}</a></span>
-                <h3><a href={eb.file} target="_blank" rel="noreferrer">{eb.judul}</a></h3>
+            <div className="jl_topik_center blog-style-one blog-small-grid">
+              <div className="jl_topik_center_w jl_radus_e" style={{ maxHeight: 350 }}>
+                <div className="jl_f_img_bg" style={{ backgroundImage: `url(${eb.thumb})` }} />
+                <a href={eb.file} target="_blank" rel="noreferrer" className="jl_f_img_link" title="Baca Buku" />
+                <div className="text-box">
+                  <div className="jl_post_meta">
+                    <span className="jl_post_meta jl_f_cat">
+                      <a style={{ background: "#305b90" }} className="post-category-color-text">{eb.kategori}</a>
+                    </span>
+                  </div>
+                  <h3><a href={eb.file} target="_blank" rel="noreferrer">{eb.judul}</a></h3>
+                </div>
               </div>
             </div>
           </div>
@@ -148,23 +177,25 @@ export default function Home() {
 
       {/* Layout dua kolom: konten (kiri sudah selesai di atas) + sidebar kanan */}
       <div className="row">
-        <div className="col-md-8" />
-        <div className="col-md-4">
+        <div className="col-md-8 col-sm-12" />
+        <div className="col-md-4 col-sm-12">
           <div className="section-title">
             <h1 className="text-uppercase"><Link to="/opini">Kepala Dinas</Link></h1>
           </div>
-          <div className="card p-0 shadow-sm text-center">
+          <div className="card p-0 shadow-sm">
             <div className="card-body p-1">
-              <img
-                src={kepalaDinas.foto}
-                alt={kepalaDinas.nama}
-                className="pointer"
-                onClick={() => setSambutanOpen(true)}
-              />
-              <br />
-              <span className="badge badge-light-primary profile-badge">
-                {kepalaDinas.nama} <i className="fas fa-arrow-right" />
-              </span>
+              <div className="justify-content-between align-items-center text-center">
+                <img
+                  src={kepalaDinas.foto}
+                  alt={kepalaDinas.nama}
+                  className="pointer"
+                  onClick={() => setSambutanOpen(true)}
+                />
+                <br />
+                <span className="badge badge-light-primary profile-badge text-center">
+                  {kepalaDinas.nama} <i className="fas fa-arrow-right" />
+                </span>
+              </div>
             </div>
           </div>
 
@@ -204,7 +235,9 @@ export default function Home() {
             width="100%"
             height="250"
             style={{ border: 0 }}
+            allowFullScreen
             loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
             title="Peta Kantor"
           />
         </div>
