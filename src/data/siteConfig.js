999ce78ec852e72d7213a8b0a34c72686761950f
Muhamad Soleh <solehudinz3692@gmail.com>
2026-09-12 22:10:57 +0700

    Update site configuration with new phone and map details
    Updated phone number and added new fields for vision and map URLs.

--- src/data/siteConfig.js
+++ src/data/siteConfig.js
@@ -5,7 +5,7 @@ export const siteConfig = {
   favicon: "/img/konfigurasi/icon/tm2.png",
   address:
     "Kp.Jungkel, Ds.Tanjakan Mekar, Kec.Rajeg, Kab. Tangerang - Banten 15540",
-  phone: "+62887433130861",
+  phone: "+629690801524",
   email: "solehudinz3692@gmail.com",
   social: {
     facebook: "https://www.facebook.com/soleh.muhamad",
@@ -26,6 +26,9 @@ export const siteConfig = {
       "Meningkatkan tatakelola pemerintah yang baik dan bersih",
     ],
   },
+  visiMisiQuote:
+    "\u201cMewujudkan Kabupaten Tangerang sebagai kota budaya yang Modern, Tangguh, Gesit, Kreatif dan Sejahtera\u201d",
+  mapUrl: "https://maps.app.goo.gl/6qWQbjRqFM63ZvuB8?g_st=ac",
   mapEmbedUrl:
     "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15868.838416887245!2d106.532609!3d-6.1024512!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e41ff38cdee3d79%3A0xa8ebc179ed208332!2sWarung%20Emak%20Ati!5e0!3m2!1sid!2sid!4v1783157829397!5m2!1sid!2sid",
 };
