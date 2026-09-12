99d2abadde15b9debd38610e2bb4bfab8263d593
Muhamad Soleh <solehudinz3692@gmail.com>
2026-09-12 21:58:31 +0700

    Add fotoList data for gallery images

---
+++ src/data/fotoData.js
@@ -0,0 +1,44 @@
+export const fotoList = [
+  {
+    id: 80,
+    image: "/img/galeri/foto/41.webp",
+    judul: "Tampilan depan",
+    tanggal: "Kamis, 05 Maret 2026",
+    kategori: "Website Desa",
+  },
+  {
+    id: 79,
+    image: "/img/galeri/foto/42.webp",
+    judul: "Konfigurasi",
+    tanggal: "Kamis, 05 Maret 2026",
+    kategori: "Website Desa",
+  },
+  {
+    id: 77,
+    image: "/img/galeri/foto/43.webp",
+    judul: "Tampilan Home NeoGoe",
+    tanggal: "Selasa, 03 Maret 2026",
+    kategori: "Kegiatan Rutin",
+  },
+  {
+    id: 60,
+    image: "/img/galeri/foto/44.webp",
+    judul: "Lembata",
+    tanggal: "Selasa, 25 April 2023",
+    kategori: "Kegiatan Rutin",
+  },
+  {
+    id: 59,
+    image: "/img/galeri/foto/45.jpg",
+    judul: "Tampilan Konfigurasi Versi awal Website Desa",
+    tanggal: "Sabtu, 16 April 2022",
+    kategori: "Website Desa",
+  },
+  {
+    id: 58,
+    image: "/img/galeri/foto/46.jpg",
+    judul: "Tampilan Dashboard CMS versi lama",
+    tanggal: "Sabtu, 16 April 2022",
+    kategori: "Website Desa",
+  },
+];
