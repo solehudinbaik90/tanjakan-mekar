59b05218ec48d9c799c8ba0fc4cf292162837163
Muhamad Soleh <solehudinz3692@gmail.com>
2026-09-12 22:18:22 +0700

    Fix quotation marks in siteConfig.js

--- src/data/siteConfig.js
+++ src/data/siteConfig.js
@@ -27,7 +27,7 @@ export const siteConfig = {
     ],
   },
   visiMisiQuote:
-    "\u201cMewujudkan Kabupaten Tangerang sebagai kota budaya yang Modern, Tangguh, Gesit, Kreatif dan Sejahtera\u201d",
+    "Mewujudkan Kabupaten Tangerang sebagai kota budaya yang Modern, Tangguh, Gesit, Kreatif dan Sejahtera",
   mapUrl: "https://maps.app.goo.gl/6qWQbjRqFM63ZvuB8?g_st=ac",
   mapEmbedUrl:
     "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15868.838416887245!2d106.532609!3d-6.1024512!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e41ff38cdee3d79%3A0xa8ebc179ed208332!2sWarung%20Emak%20Ati!5e0!3m2!1sid!2sid!4v1783157829397!5m2!1sid!2sid",
