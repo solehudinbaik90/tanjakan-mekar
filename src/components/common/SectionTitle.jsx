9770006913d8756f0a52cc8368d8d2ec47da27f8
Muhamad Soleh <solehudinz3692@gmail.com>
2026-09-14 16:48:10 +0700

    Add showMore prop to SectionTitle component

--- src/components/common/SectionTitle.jsx
+++ src/components/common/SectionTitle.jsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom";
 
-export default function SectionTitle({ title, to }) {
+export default function SectionTitle({ title, to, showMore = true }) {
+  const hasMoreLink = showMore && Boolean(to);
+
   return (
     <div className="section-title">
-      <h1 className="d-flex justify-content-between">
+      <h1 className={hasMoreLink ? "d-flex justify-content-between" : ""}>
         <Link className="text-uppercase" to={to || "#"}>
           {title}
         </Link>
-        {to && (
+        {hasMoreLink && (
           <span className="pr-2" style={{ fontSize: 14, paddingTop: 2 }}>
             <Link to={to}>
               MORE <i className="fas fa-chevron-right" />
