02848898d19356a4d439478141b4f9379368c99d
Muhamad Soleh <solehudinz3692@gmail.com>
2026-09-15 16:02:10 +0700

    Update ebook data with new entries and file paths

--- src/data/ebookData.js
+++ src/data/ebookData.js
@@ -1,4 +1,37 @@
 export const ebookList = [
-  { id: 8, kategori: "Informasi Publik", judul: "Ebook Author", thumb: "/img/ebook/thumb/1.png", file: "/img/ebook/bacabuku.pdf" },
-  { id: 7, kategori: "Informasi Publik", judul: "Internet Marketing", thumb: "/img/ebook/thumb/2.png", file: "/img/ebook/bacabuku2.pdf" },
+  {
+    id: 8,
+    kategori: "Informasi Publik",
+    judul: "Ebook Author",
+    thumb: "/img/ebook/thumb1.png",
+    file: "/img/ebook/buku1.pdf",
+  },
+  {
+    id: 7,
+    kategori: "Informasi Publik",
+    judul: "Internet Marketing",
+    thumb: "/img/ebook/thumb2.png",
+    file: "/img/ebook/buku2.pdf",
+  },
+  {
+    id: 6,
+    kategori: "Cerita Rakyat",
+    judul: "Kumpulan Cerita Rakyat Nusa Tenggara Timur",
+    thumb: "/img/ebook/thumb3.jpg",
+    file: "/img/ebook/buku3.pdf",
+  },
+  {
+    id: 5,
+    kategori: "Informasi Publik",
+    judul: "Panduan Keterbukaan Informasi Publik",
+    thumb: "/img/ebook/thumb4.jpg",
+    file: "/img/ebook/buku4.pdf",
+  },
+  {
+    id: 2,
+    kategori: "Informasi Publik",
+    judul: "Buku Saku Layanan Informasi Publik",
+    thumb: "/img/ebook/thumb5.jpg",
+    file: "/img/ebook/buku5.pdf",
+  },
 ];
