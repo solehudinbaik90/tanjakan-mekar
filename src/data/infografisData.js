20af13b507d7423253849c994717bc76822e0141
Muhamad Soleh <solehudinz3692@gmail.com>
2026-09-15 16:05:08 +0700

    Revise infografis data and add full list
    Updated infografisList with new image paths and added infografisFullList with detailed entries.

--- src/data/infografisData.js
+++ src/data/infografisData.js
@@ -1,8 +1,42 @@
 export const infografisList = [
-  { id: 5, image: "/img/informasi/infografis/1.png" },
-  { id: 4, image: "/img/informasi/infografis/2.jpg" },
-  { id: 2, image: "/img/informasi/infografis/3.jpeg" },
-  { id: 9, image: "/img/informasi/infografis/4.png" },
-  { id: 1, image: "/img/informasi/infografis/5.jpg" },
-  { id: 6, image: "/img/informasi/infografis/6.jpeg" },
+  { id: 5, image: "/img/informasi/infografis/info1.png" },
+  { id: 4, image: "/img/informasi/infografis/info2.jpg" },
+  { id: 2, image: "/img/informasi/infografis/info3.jpeg" },
+  { id: 9, image: "/img/informasi/infografis/info4.png" },
+  { id: 1, image: "/img/informasi/infografis/info5.jpg" },
+  { id: 6, image: "/img/informasi/infografis/info6.jpeg" },
+];
+
+
+export const infografisFullList = [
+  {
+    id: 38,
+    judul: "KOMDIGI akan batasi Usia Anak bikin Akun Medsos",
+    image: "/img/informasi/infografis/info7.png",
+  },
+  {
+    id: 9,
+    judul: "Efek Negatif Teknologi Digital pada Anak",
+    image: "/img/informasi/infografis/info8.png",
+  },
+  {
+    id: 7,
+    judul: "Manfaat Teknologi Digital bagi Pendidikan Anak",
+    image: "/img/informasi/infografis/info9.jpg",
+  },
+  {
+    id: 6,
+    judul: "Tumbuh Kembang Anak Media Digital",
+    image: "/img/informasi/infografis/info10.jpeg",
+  },
+  {
+    id: 5,
+    judul: "Ekonomi Digital Untuk Indonesia Lebih Sejahtera",
+    image: "/img/informasi/infografis/info11.png",
+  },
+  {
+    id: 4,
+    judul: "Olahraga adalah ibadah",
+    image: "/img/informasi/infografis/info12.jpg",
+  },
 ];
