20b6ffa2c85f1febd26f5abceb3b06a77eceb2e8
Muhamad Soleh <solehudinz3692@gmail.com>
2026-09-15 16:39:53 +0700

    Add new routes and components to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,21 @@ import StrukturOrganisasi from "./pages/StrukturOrganisasi.jsx";
 import TugasPokokFungsi from "./pages/TugasPokokFungsi.jsx";
 import Pegawai from "./pages/Pegawai.jsx";
 import Berita from "./pages/Berita.jsx";
+import BeritaDetail from "./pages/BeritaDetail.jsx";
+import KategoriBerita from "./pages/KategoriBerita.jsx";
 import Layanan from "./pages/Layanan.jsx";
 import Pengumuman from "./pages/Pengumuman.jsx";
 import Agenda from "./pages/Agenda.jsx";
+import BankData from "./pages/BankData.jsx";
 import Foto from "./pages/Foto.jsx";
 import Video from "./pages/Video.jsx";
 import MasukanSaran from "./pages/MasukanSaran.jsx";
 import Survey from "./pages/Survey.jsx";
 import BukuTamu from "./pages/BukuTamu.jsx";
+import ProdukHukum from "./pages/ProdukHukum.jsx";
+import Infografis from "./pages/Infografis.jsx";
+import Transparansi from "./pages/Transparansi.jsx";
+import Ebook from "./pages/Ebook.jsx";
 import NotFound from "./pages/NotFound.jsx";
 
 export default function App() {
@@ -30,11 +37,18 @@ export default function App() {
         <Route path="/layanan" element={<Layanan />} />
         <Route path="/pengumuman" element={<Pengumuman />} />
         <Route path="/agenda" element={<Agenda />} />
+        <Route path="/bankdata" element={<BankData />} />
         <Route path="/foto" element={<Foto />} />
         <Route path="/video" element={<Video />} />
         <Route path="/masukansaran" element={<MasukanSaran />} />
         <Route path="/survey" element={<Survey />} />
         <Route path="/bukutamu" element={<BukuTamu />} />
+        <Route path="/produkhukum" element={<ProdukHukum />} />
+        <Route path="/infografis" element={<Infografis />} />
+        <Route path="/transparansi" element={<Transparansi />} />
+        <Route path="/ebook" element={<Ebook />} />
+        <Route path="/category/:kategoriSlug" element={<KategoriBerita />} />
+        <Route path="/:slug" element={<BeritaDetail />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
