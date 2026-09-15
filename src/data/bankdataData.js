a324e77b8d2b25436cec5f9b2e20566a4e2e76c2
Muhamad Soleh <solehudinz3692@gmail.com>
2026-09-15 16:00:06 +0700

    Create bankdataData.js with bankdataList export
    Added bankdataList containing various reports with IDs, titles, dates, and file paths.

---
+++ src/data/bankdataData.js
@@ -0,0 +1,25 @@
+export const bankdataList = [
+  {
+    id: 1,
+    judul: "Laporan hasil pelaksanaan rencana strategis dan rencana kerja Dinas",
+    tanggal: "04 Juni 2021",
+    file:
+      "/img/bankdata/bank1.pdf",
+  },
+  {
+    id: 2,
+    judul: "Rumusan kebijakan teknis bidang keolahragaan",
+    tanggal: "04 Juni 2021",
+    file:
+      "/img/bankdata/bank2.pdf",
+  },
+  {
+    id: 3,
+    judul: "Rencana dan program kerja pembinaan organisasi kepemudaan",
+    tanggal: "04 Juni 2021",
+    file:
+      "/img/bankdata/bank3.pdf",
+  },
+];
+
+export default bankdataList;
