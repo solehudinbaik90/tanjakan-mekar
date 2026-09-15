28a52c29290ad4a6057cedc2fce1668e2af16060
Muhamad Soleh <solehudinz3692@gmail.com>
2026-09-15 16:11:41 +0700

    Update image paths and articles in beritaData
    Updated image paths for beritaUtama and beritaSamping. Added new articles to artikelList and updated existing articles with new details.

--- src/data/beritaData.js
+++ src/data/beritaData.js
@@ -1,5 +1,6 @@
 export const beritaUtama = {
-  image: "/img/informasi/berita/1.jpg",
+  image:
+    "/img/informasi/berita/news1.jpg",
   kategori: "Berita Dinas",
   kategoriSlug: "berita-dinas",
   tanggal: "Rabu, 11 Maret 2026",
@@ -9,7 +10,8 @@ export const beritaUtama = {
 
 export const beritaSamping = [
   {
-    image: "/img/informasi/berita/2.jpeg",
+    image:
+      "/img/informasi/berita/news2.jpeg",
     judul: "Cerdas Terima Informasi, Masyarakat Perlu Diedukasi",
     slug: "cerdas-terima-informasi-masyarakat-perlu-diedukasi",
     kategori: "Berita Dinas",
@@ -17,7 +19,8 @@ export const beritaSamping = [
     tanggal: "Rabu, 11 Maret 2026",
   },
   {
-    image: "/img/informasi/berita/3.jpg",
+    image:
+      "/img/informasi/berita/news3.jpg",
     judul: "Terinspirasi dari Sang Ibu, Lody Lontoh Jadi Master Aerobik Indonesia",
     slug: "terinspirasi-dari-sang-ibu-lody-lontoh-jadi-master-aerobik-indonesia",
     kategori: "Inspirasi",
@@ -25,7 +28,8 @@ export const beritaSamping = [
     tanggal: "Sabtu, 07 Maret 2026",
   },
   {
-    image: "/img/informasi/berita/4.png",
+    image:
+      "/img/informasi/berita/news4.png",
     judul: "Soroti Tubuh Politik Bupati Sunur, Ini Yang DiKatakan Aktivis ARBL",
     slug: "soroti-tubuh-politik-bupati-sunur-ini-yang-dikatakan-aktivis-arbl",
     kategori: "Berita Dinas",
@@ -33,7 +37,8 @@ export const beritaSamping = [
     tanggal: "Sabtu, 04 Desember 2021",
   },
   {
-    image: "/img/informasi/berita/5.jpeg",
+    image:
+      "/img/informasi/berita/news5.jpeg",
     judul: "Menpora Amali Hadiri Penyampaian LHP LKPP 2020 Secara Virtual",
     slug: "menpora-amali-hadiri-penyampaian-lhp-lkpp-2020-secara-virtual",
     kategori: "Berita Dinas",
@@ -42,113 +47,326 @@ export const beritaSamping = [
   },
 ];
 
+// Section "ARTIKEL" di homepage — 7 item, kategori Artikel
 export const artikelList = [
   {
-    image: "/img/informasi/berita/51.png",
-    judul: "Resmikan Masjid Mekar, Wakil Bupati MBD Ajak Perkuat Sinergitas",
-    slug: "resmikan-mesjid-mekar-wakil-bupati-mbd-ajak-perkuat-sinergitas",
-    penulis: "Vian Taum",
-    kategori: "Artikel",
-    kategoriSlug: "artikel",
-    tanggal: "Minggu, 01 Maret 2026",
-  },
-  {
-    image: "/img/informasi/berita/52.webp",
-    judul: "Dibalik Nyala Obor Toleransi NKRI Yang Disulut Bupati Kanis Tuaq",
-    slug: "dibalik-nyala-obor-toleransi-nkri-yang-disulut-bupati-kanis-tuaq",
-    penulis: "Vian Taum",
-    kategori: "Artikel",
-    kategoriSlug: "artikel",
-    tanggal: "Sabtu, 21 Februari 2026",
-  },
-  {
-    image: "/img/informasi/berita/53.jpg",
-    judul: "Pemda Lembata Pastikan Distribusi BBM Aman Terkendali, Warga Diimbau Tenang",
-    slug: "pemda-lembata-pastikan-distribusi-bbm-aman-terkendali-warga-diimbau-tenang",
-    penulis: "Vian Taum",
-    kategori: "Artikel",
-    kategoriSlug: "artikel",
-    tanggal: "Sabtu, 21 Februari 2026",
-  },
-  {
-    image: "/img/informasi/berita/54.jpg",
-    judul: "Literasi Digital Bergulir ke Seluruh Negeri",
-    slug: "literasi-digital-bergulir-ke-seluruh-negeri",
-    penulis: "Vian Taum",
-    kategori: "Artikel",
-    kategoriSlug: "artikel",
-    tanggal: "Kamis, 08 Januari 2026",
-  },
-];
-
-// Daftar lengkap untuk halaman /berita (bisa diganti fetch API paginasi di masa depan)
-export const beritaList = [
-  {
-    image: "/img/media/13.webp",
-    judul: "Website Desa Baru telah Hadir dengan Fitur Media Library untuk Pengelolaan File Lebih Efisien",
+    image:
+      "/img/informasi/berita/news6.webp",
+    judul:
+      "CMS Datagoe Versi 6 Hadir dengan Fitur Media Library untuk Pengelolaan File Lebih Efisien",
     slug: "cms-datagoe-versi-6-hadir-dengan-fitur-media-library-untuk-pengelolaan-file-lebih-efisien",
-    penulis: "Vian Taum",
+    penulis: "Msoleh",
     kategori: "Artikel",
     kategoriSlug: "artikel",
     tanggal: "Kamis, 12 Maret 2026",
     ringkasan:
-      "Fitur Media Library memungkinkan administrator dan pengelola konten untuk menyimpan, mengelola, serta menggunakan kembali file media gambar secara lebih terstruktur....",
+      "Fitur Media Library memungkinkan administrator dan pengelola konten untuk menyimpan, mengelola, serta menggunakan kembali file media gambar secara lebih terstruktur.",
   },
   {
-    image: "/img/informasi/berita/14.png",
-    judul: "Pengaruh Kemajuan Teknologi Komunikasi dan Informasi Terhadap Karakter Anak",
+    image:
+      "/img/informasi/berita/news7.png",
+    judul:
+      "Pengaruh Kemajuan Teknologi Komunikasi dan Informasi Terhadap Karakter Anak",
     slug: "pengaruh-kemajuan-teknologi-komunikasi-dan-informasi-terhadap-karakter-anak",
-    penulis: "Vian Taum",
+    penulis: "Msoleh",
     kategori: "Artikel",
     kategoriSlug: "artikel",
     tanggal: "Rabu, 11 Maret 2026",
     ringkasan:
-      "Kehidupan manusia yang bermula dari kesederhanaan kini menjadi kehidupan yang bisa dikategorikan sangat modern...",
+      "Kehidupan manusia yang bermula dari kesederhanaan kini menjadi kehidupan yang bisa dikategorikan sangat modern seiring kemajuan teknologi informasi dan komunikasi.",
   },
   {
-    image: "/img/informasi/berita/15.jpg",
+    image:
+      "/img/informasi/berita/news8.png",
+    judul: "Resmikan Gereja Regoha, Wakil Bupati MBD Ajak Perkuat Sinergitas",
+    slug: "resmikan-gereja-regoha-wakil-bupati-mbd-ajak-perkuat-sinergitas",
+    penulis: "Msoleh",
+    kategori: "Artikel",
+    kategoriSlug: "artikel",
+    tanggal: "Minggu, 01 Maret 2026",
+    ringkasan:
+      "Wakil Bupati MBD berharap kolaborasi dan sinergitas antara GPM dan pemerintah tetap diperkuat guna pembangunan daerah yang semakin maju.",
+  },
+  {
+    image:
+      "/img/informasi/berita/news9.webp",
+    judul: "Dibalik Nyala Obor Toleransi NKRI Yang Disulut Bupati Kanis Tuaq",
+    slug: "dibalik-nyala-obor-toleransi-nkri-yang-disulut-bupati-kanis-tuaq",
+    penulis: "Msoleh",
+    kategori: "Artikel",
+    kategoriSlug: "artikel",
+    tanggal: "Sabtu, 21 Februari 2026",
+    ringkasan:
+      "Forum Kerukunan Umat Beragama Kabupaten Lembata menggelar peringatan Hari Toleransi Internasional dengan menyalakan Obor NKRI sebagai simbol persatuan.",
+  },
+  {
+    image:
+      "/img/informasi/berita/news10.jpg",
+    judul:
+      "Pemda Lembata Pastikan Distribusi BBM Aman Terkendali, Warga Diimbau Tenang",
+    slug: "pemda-lembata-pastikan-distribusi-bbm-aman-terkendali-warga-diimbau-tenang",
+    penulis: "Msoleh",
+    kategori: "Artikel",
+    kategoriSlug: "artikel",
+    tanggal: "Sabtu, 21 Februari 2026",
+    ringkasan:
+      "Pemerintah Kabupaten Lembata memastikan kelangkaan BBM bersifat insidentil akibat cuaca buruk, bukan karena terhentinya pasokan dari Pertamina.",
+  },
+  {
+    image:
+      "/img/informasi/berita/news11.jpg",
+    judul: "Politik Digital Anak Muda",
+    slug: "politik-digital-anak-muda",
+    penulis: "Msoleh",
+    kategori: "Artikel",
+    kategoriSlug: "artikel",
+    tanggal: "Kamis, 08 Januari 2026",
+    ringkasan:
+      "Generasi Z dan Milenial menjadi kekuatan tersendiri dalam kontestasi politik seiring pemanfaatan media sosial untuk kampanye dan partisipasi politik.",
+  },
+  {
+    image:
+      "/img/informasi/berita/news12.jpg",
+    judul: "Literasi Digital Bergulir ke Seluruh Negeri",
+    slug: "literasi-digital-bergulir-ke-seluruh-negeri",
+    penulis: "Msoleh",
+    kategori: "Artikel",
+    kategoriSlug: "artikel",
+    tanggal: "Kamis, 08 Januari 2026",
+    ringkasan:
+      "Tantangan di ruang digital semakin besar; hoaks, penipuan daring, dan ujaran kebencian perlu diwaspadai lewat Gerakan Nasional Literasi Digital.",
+  },
+];
+
+// Daftar lengkap untuk halaman /berita — diurutkan dari yang terbaru
+export const beritaList = [
+  ...artikelList.slice(0, 1),
+  {
+    image:
+      "/img/informasi/berita/news13.jpg",
     judul: "Lantik Karo Perencanaan dan Organisasi, Ini Pesan Menpora Amali",
     slug: "lantik-karo-perencanaan-dan-organisasi-ini-pesan-menpora-amali",
-    penulis: "Vian Taum",
+    penulis: "Msoleh",
     kategori: "Berita Dinas",
     kategoriSlug: "berita-dinas",
     tanggal: "Rabu, 11 Maret 2026",
-    ringkasan: "Menpora RI Zainudin Amali melantik Kepala Biro Perencanaan dan Organisasi Kemenpora...",
+    ringkasan:
+      "Menpora RI Zainudin Amali melantik Kepala Biro Perencanaan dan Organisasi Kemenpora, Sri Wahyuni, secara virtual dan berpesan agar segera beradaptasi.",
   },
   {
-    image: "/img/informasi/berita/16.jpeg",
+    image:
+      "/img/informasi/berita/news14.jpeg",
     judul: "Cerdas Terima Informasi, Masyarakat Perlu Diedukasi",
     slug: "cerdas-terima-informasi-masyarakat-perlu-diedukasi",
-    penulis: "Vian Taum",
+    penulis: "Msoleh",
     kategori: "Berita Dinas",
     kategoriSlug: "berita-dinas",
     tanggal: "Rabu, 11 Maret 2026",
-    ringkasan: "Kemajuan teknologi informasi, khususnya melalui platform digital, terjadi sangat pesat belakangan ini...",
+    ringkasan:
+      "Wapres Ma'ruf Amin menegaskan pentingnya edukasi masyarakat agar cerdas menyaring informasi di tengah derasnya arus disrupsi informasi digital.",
   },
+  artikelList[1],
   {
-    image: "/img/informasi/berita/17.jpg",
+    image:
+      "/img/informasi/berita/news15.jpg",
     judul: "Terinspirasi dari Sang Ibu, Lody Lontoh Jadi Master Aerobik Indonesia",
     slug: "terinspirasi-dari-sang-ibu-lody-lontoh-jadi-master-aerobik-indonesia",
-    penulis: "Vian Taum",
+    penulis: "Msoleh",
     kategori: "Inspirasi",
     kategoriSlug: "inspirasi",
     tanggal: "Sabtu, 07 Maret 2026",
-    ringkasan: "Perjalanannya dimulai saat ia masih berusia anak-anak, tepatnya diusia 7 tahun...",
+    ringkasan:
+      "Perjalanan Lody Lontoh menekuni senam aerobik sejak usia 7 tahun berkat dorongan sang ibu, hingga menjadi master aerobik dan pelatih nasional Indonesia.",
+  },
+  artikelList[2],
+  artikelList[3],
+  artikelList[4],
+  artikelList[5],
+  artikelList[6],
+  {
+    image:
+      "/img/informasi/berita/news16.png",
+    judul:
+      "Pemerintah Terbitkan Perpres tentang Stranas Percepatan Pembangunan Daerah Tertinggal",
+    slug: "pemerintah-terbitkan-perpres-tentang-stranas-percepatan-pembangunan-daerah-tertinggal",
+    penulis: "Ama Deril Taum",
+    kategori: "Nasional",
+    kategoriSlug: "nasional",
+    tanggal: "Jumat, 08 Juli 2022",
+    ringkasan:
+      "Presiden Jokowi menandatangani Perpres Nomor 105 Tahun 2021 tentang Strategi Nasional Percepatan Pembangunan Daerah Tertinggal periode 2020–2024.",
+  },
+  {
+    image:
+      "/img/informasi/berita/news17.png",
+    judul: "Soroti Tubuh Politik Bupati Sunur, Ini Yang DiKatakan Aktivis ARBL",
+    slug: "soroti-tubuh-politik-bupati-sunur-ini-yang-dikatakan-aktivis-arbl",
+    penulis: "Msoleh",
+    kategori: "Berita Dinas",
+    kategoriSlug: "berita-dinas",
+    tanggal: "Sabtu, 04 Desember 2021",
+    ringkasan:
+      "Kunjungan Menteri Kominfo menjadi sorotan aktivis ARBL terkait dinamika politik di daerah.",
+  },
+  {
+    image:
+      "/img/informasi/berita/news18.jpeg",
+    judul: "Buka Peluang Ekonomi Kreatif dengan Infrastuktur dan Talenta Digital",
+    slug: "buka-peluang-ekonomi-kreatif-dengan-infrastuktur-dan-talenta-digital",
+    penulis: "Msoleh",
+    kategori: "Berita Dinas",
+    kategoriSlug: "berita-dinas",
+    tanggal: "Senin, 28 Juni 2021",
+    ringkasan:
+      "Kepala BRIN LT. Handoko menilai infrastruktur 5G dapat membuka peluang ekonomi kreatif berbasis inovasi digital lewat penguatan talenta digital.",
+  },
+  {
+    image:
+      "/img/informasi/berita/news19.jpeg",
+    judul:
+      "Raih WTP Untuk Kelima Kalinya, Presiden: Kita Ingin Gunakan Uang Rakyat dengan Baik",
+    slug: "raih-wtp-untuk-kelima-kalinya-presiden-kita-ingin-gunakan-uang-rakyat-dengan-baik",
+    penulis: "Msoleh",
+    kategori: "Berita Dinas",
+    kategoriSlug: "berita-dinas",
+    tanggal: "Jumat, 25 Juni 2021",
+    ringkasan:
+      "Presiden Jokowi menerima opini WTP kelima berturut-turut atas LKPP 2020 dari BPK dan menegaskan komitmen mengelola anggaran secara transparan.",
+  },
+  {
+    image:
+      "/img/informasi/berita/news20.jpeg",
+    judul:
+      "LKPP 2020 Raih WTP, Ini Harapan Presiden Jokowi kepada Pimpinan Kementerian/Lembaga",
+    slug: "lkpp-2020-raih-wtp-ini-harapan-presiden-jokowi-kepada-pimpinan-kementerianlembaga",
+    penulis: "Msoleh",
+    kategori: "Berita Dinas",
+    kategoriSlug: "berita-dinas",
+    tanggal: "Jumat, 25 Juni 2021",
+    ringkasan:
+      "Presiden Jokowi mengapresiasi BPK atas opini WTP LKPP 2020 dan meminta seluruh kementerian/lembaga segera menindaklanjuti rekomendasi pemeriksaan.",
+  },
+  {
+    image:
+      "/img/informasi/berita/news21.jpeg",
+    judul: "Menpora Amali Hadiri Penyampaian LHP LKPP 2020 Secara Virtual",
+    slug: "menpora-amali-hadiri-penyampaian-lhp-lkpp-2020-secara-virtual",
+    penulis: "Msoleh",
+    kategori: "Berita Dinas",
+    kategoriSlug: "berita-dinas",
+    tanggal: "Jumat, 25 Juni 2021",
+    ringkasan:
+      "Menpora Zainudin Amali menghadiri penyampaian LHP LKPP dan IHPS II Tahun 2020 dari BPK secara virtual dari Kantor Kemenpora.",
+  },
+  {
+    image:
+      "/img/informasi/berita/news22.jpg",
+    judul:
+      "Jalani Vaksinasi, Pebulutangkis Melati Oktavianti: Rasanya Seperti Digigit Semut",
+    slug: "jalani-vaksinasi-pebulutangkis-melati-oktavianti-rasanya-seperti-digigit-semut",
+    penulis: "Msoleh",
+    kategori: "Inspirasi",
+    kategoriSlug: "inspirasi",
+    tanggal: "Senin, 31 Mei 2021",
+    ringkasan:
+      "Pebulutangkis Melati Daeva Oktavianti menjalani vaksinasi Covid-19 di Istora Senayan dan mengajak masyarakat untuk tidak takut divaksin.",
+  },
+  {
+    image:
+      "/img/informasi/berita/news23.jpg",
+    judul:
+      "Menpora Puji Ketum FPTI Yenny Wahid atas Prestasi Dua Atlet Panjat Tebing Indonesia",
+    slug: "menpora-puji-ketum-fpti-yenny-wahid-atas-prestasi-dua-atlet-panjat-tebing-indonesia",
+    penulis: "Msoleh",
+    kategori: "Olahraga",
+    kategoriSlug: "olahraga",
+    tanggal: "Senin, 31 Mei 2021",
+    ringkasan:
+      "Veddriq Leonardo dan Kiromal Katibin memecahkan rekor dunia nomor speed 15 meter pada IFSC World Cup, Menpora memuji kerja keras Ketum FPTI Yenny Wahid.",
+  },
+  {
+    image:
+      "/img/informasi/berita/news24.jpeg",
+    judul: "Menpora: Persiapan PON XX Berjalan sesuai Rencana",
+    slug: "menpora-persiapan-pon-xx-berjalan-sesuai-rencana",
+    penulis: "Msoleh",
+    kategori: "Berita Dinas",
+    kategoriSlug: "berita-dinas",
+    tanggal: "Selasa, 25 Mei 2021",
+    ringkasan:
+      "Menpora Zainudin Amali memastikan persiapan fisik dan non-fisik penyelenggaraan PON XX Papua berjalan sesuai rencana dan jadwal.",
+  },
+  {
+    image:
+      "/img/informasi/berita/news25.jpeg",
+    judul: "Pemerintah Dorong Pemanfaatan 5G untuk Industri Dalam Negeri",
+    slug: "pemerintah-dorong-pemanfaatan-5g-untuk-industri-dalam-negeri",
+    penulis: "Msoleh",
+    kategori: "Berita Dinas",
+    kategoriSlug: "berita-dinas",
+    tanggal: "Senin, 24 Mei 2021",
+    ringkasan:
+      "Kemenperin mendorong industri dalam negeri memproduksi perangkat pendukung base station 5G guna menunjang transformasi digital Indonesia.",
   },
 ];
 
-export const kategoriBerita = [
-  { nama: "Artikel", slug: "artikel", jumlah: 7 },
-  { nama: "Berita Dinas", slug: "berita-dinas", jumlah: 9 },
-  { nama: "Inspirasi", slug: "inspirasi", jumlah: 2 },
-  { nama: "Nasional", slug: "nasional", jumlah: 1 },
-  { nama: "Olahraga", slug: "olahraga", jumlah: 1 },
-];
+// Jumlah tiap kategori — dihitung ulang dari beritaList agar selalu akurat,
+// bukan angka statis yang gampang basi kalau ada penambahan/penghapusan data.
+export const kategoriBerita = Object.values(
+  beritaList.reduce((acc, item) => {
+    if (!acc[item.kategoriSlug]) {
+      acc[item.kategoriSlug] = {
+        nama: item.kategori,
+        slug: item.kategoriSlug,
+        jumlah: 0,
+      };
+    }
+    acc[item.kategoriSlug].jumlah += 1;
+    return acc;
+  }, {})
+);
 
 export const terpopuler = [
-  { judul: "Pengaruh Kemajuan Teknologi Komunikasi dan Informasi Terhadap Karakter Anak", slug: "pengaruh-kemajuan-teknologi-komunikasi-dan-informasi-terhadap-karakter-anak", kategori: "Artikel", tanggal: "Rabu, 11 Maret 2026" },
-  { judul: "Soroti Tubuh Politik Bupati Sunur, Ini Yang DiKatakan Aktivis ARBL", slug: "soroti-tubuh-politik-bupati-sunur-ini-yang-dikatakan-aktivis-arbl", kategori: "Berita Dinas", tanggal: "Sabtu, 04 Desember 2021" },
-  { judul: "Lantik Karo Perencanaan dan Organisasi, Ini Pesan Menpora Amali", slug: "lantik-karo-perencanaan-dan-organisasi-ini-pesan-menpora-amali", kategori: "Berita Dinas", tanggal: "Rabu, 11 Maret 2026" },
-  { judul: "Pemerintah Terbitkan Perpres tentang Stranas Percepatan Pembangunan Daerah Tertinggal", slug: "pemerintah-terbitkan-perpres-tentang-stranas-percepatan-pembangunan-daerah-tertinggal", kategori: "Nasional", tanggal: "Jumat, 08 Juli 2022" },
-  { judul: "Literasi Digital Bergulir ke Seluruh Negeri", slug: "literasi-digital-bergulir-ke-seluruh-negeri", kategori: "Artikel", tanggal: "Kamis, 08 Januari 2026" },
+  {
+    judul:
+      "Pengaruh Kemajuan Teknologi Komunikasi dan Informasi Terhadap Karakter Anak",
+    slug: "pengaruh-kemajuan-teknologi-komunikasi-dan-informasi-terhadap-karakter-anak",
+    kategori: "Artikel",
+    tanggal: "Rabu, 11 Maret 2026",
+  },
+  {
+    judul: "Soroti Tubuh Politik Bupati Sunur, Ini Yang DiKatakan Aktivis ARBL",
+    slug: "soroti-tubuh-politik-bupati-sunur-ini-yang-dikatakan-aktivis-arbl",
+    kategori: "Berita Dinas",
+    tanggal: "Sabtu, 04 Desember 2021",
+  },
+  {
+    judul: "Lantik Karo Perencanaan dan Organisasi, Ini Pesan Menpora Amali",
+    slug: "lantik-karo-perencanaan-dan-organisasi-ini-pesan-menpora-amali",
+    kategori: "Berita Dinas",
+    tanggal: "Rabu, 11 Maret 2026",
+  },
+  {
+    judul:
+      "Pemerintah Terbitkan Perpres tentang Stranas Percepatan Pembangunan Daerah Tertinggal",
+    slug: "pemerintah-terbitkan-perpres-tentang-stranas-percepatan-pembangunan-daerah-tertinggal",
+    kategori: "Nasional",
+    tanggal: "Jumat, 08 Juli 2022",
+  },
+  {
+    judul: "Literasi Digital Bergulir ke Seluruh Negeri",
+    slug: "literasi-digital-bergulir-ke-seluruh-negeri",
+    kategori: "Artikel",
+    tanggal: "Kamis, 08 Januari 2026",
+  },
 ];
+
+// Helper lookup — memudahkan halaman detail berita (mis. /:slug) mengambil
+// satu item tanpa harus filter manual berulang kali di tiap page.
+export function getBeritaBySlug(slug) {
+  return beritaList.find((item) => item.slug === slug) || null;
+}
+
+export function getBeritaByKategori(kategoriSlug) {
+  return beritaList.filter((item) => item.kategoriSlug === kategoriSlug);
+}
